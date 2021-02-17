71cca9cda1941525e342f01ff821277aac891a04
iZsolt <incze.zsolt@yahoo.com>
2021-02-17 13:25:06 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PROJECTS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROJECTS.dir/main.c.obj"
+  "PROJECTS.exe"
+  "PROJECTS.exe.manifest"
+  "PROJECTS.pdb"
+  "libPROJECTS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROJECTS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PROJECTS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PROJECTS.
+# This may be replaced when dependencies are built.
